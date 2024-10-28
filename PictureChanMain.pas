52d27bc3eca52da104a5c0c0aa2e38f0d0faf8c8
Alena Lisicyna <alelisicyna@gmail.com>
2024-10-28 19:27:26 +0300

    Add files via upload

--- PictureChanMain.pas
+++ PictureChanMain.pas
@@ -53,16 +53,16 @@ implementation
 
 function StreamToString(aStream: TStream): string;
 var
-  SS: TStringStream;
+  StringStream: TStringStream;
 begin
   if aStream <> nil then
   begin
-    SS := TStringStream.Create('');
+    StringStream := TStringStream.Create('');
     try
-      SS.CopyFrom(aStream, 0); // Exception: TStream.Seek not implemented
-      Result := SS.DataString;
+      StringStream.CopyFrom(aStream, 0); // Exception: TStream.Seek not implemented
+      Result := StringStream.DataString;
     finally
-      SS.Free;
+      StringStream.Free;
     end;
   end else
   begin
@@ -84,51 +84,53 @@ begin
   ColorError.Visible := False;
   LinkText := Link.Text;
 
-  if LinkText = '' then
-    // URL is empty
-  else if(LinkText <> '') then
+  if(LinkText <> '') then
   begin
-    try
-      Html := StreamToString(NetHTTPClient.Get(LinkText).ContentStream);
-    except
-      TextError.Visible := True;
-      ColorError.Visible := True;
-    end;
-    LStrStream := TStringStream.Create('', TEncoding.UTF8);
-    try
-      LHtml := ParserHTML(Html);
-      if LHtml <> nil then
-        begin // 4chan.org
-          LList := LHtml.Find('a');
-          for LHtml in LList do
-            if '.' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 3] then
-              if ('j' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 2]) or ('p' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 2]) then
-                begin
-                  // array
-                end;
-
-          { захаваць карцінкі ў ImageCollection
-            ImageCollection.Add(LHtml.Attributes['href'], NetHTTPClient.Get(LHtml.Attributes['href']).ContentStream); }
-
-        { Стары код, частка зь яго потым спатрэбіцца
-          PictureLink := 'https://www.2chan.net/b/src/' + LHtml.Find('a')[15].Text;
-          GetPicture := TPicture.Create;
-          try
-            GetPicture.LoadFromStream(NetHTTPClient.Get(PictureLink).ContentStream);
-            ImageCollection.Add('gg', GetPictrure);
-            Image.Picture := GetPicture;
-            Image.Hint := PictureLink;
-            Save.Caption := 'Save 0/1';
-            Save.Visible := True;
-          except
-            TextError.Visible := True;
-            ColorError.Visible := True;
-            GetPicture.Free;
-          end; }
+    if Pos('4chan.org', LinkText) <> 0 then
+      begin
+        Writeln('American');
+        try
+          Html := StreamToString(NetHTTPClient.Get(LinkText).ContentStream);
+        except
+          TextError.Visible := True;
+          ColorError.Visible := True;
         end;
-    except
-      LStrStream.Free;
-    end;
+        LStrStream := TStringStream.Create('', TEncoding.UTF8);
+        LHtml := ParserHTML(Html);
+        if LHtml <> nil then
+          begin // 4chan.org
+            LList := LHtml.Find('a');
+            for LHtml in LList do
+              if '.' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 3] then
+                if ('j' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 2]) or ('p' = LHtml.Attributes['href'][length(LHtml.Attributes['href']) - 2]) then
+                  begin
+                  // array
+                  end;
+
+              { захаваць карцінкі ў ImageCollection
+                ImageCollection.Add(LHtml.Attributes['href'], NetHTTPClient.Get(LHtml.Attributes['href']).ContentStream); }
+
+            { Стары код, частка зь яго потым спатрэбіцца
+              PictureLink := 'https://www.2chan.net/b/src/' + LHtml.Find('a')[15].Text;
+              GetPicture := TPicture.Create;
+              try
+                GetPicture.LoadFromStream(NetHTTPClient.Get(PictureLink).ContentStream);
+                ImageCollection.Add('gg', GetPictrure);
+                Image.Picture := GetPicture;
+                Image.Hint := PictureLink;
+                Save.Caption := 'Save 0/1';
+                Save.Visible := True;
+              except
+                TextError.Visible := True;
+                ColorError.Visible := True;
+                GetPicture.Free;
+              end; }
+          end;
+      end;
+      if Pos('2chan.net', LinkText) <> 0 then
+        begin
+          Writeln('japanese');
+        end;
   end;
 end;
 
